8a588caac8aa83a5395f829c2a3a6c81e9e9e0dd
ffrogy <120000570+ffrogy@users.noreply.github.com>
2023-06-03 22:17:21 +0900

    Create mulSum.java

---
+++ basic/mulSum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int sum = 0;
+        int mulSum = 1;
+        
+        for(int i = 0; i < num_list.length; i++){
+            sum += num_list[i];
+            mulSum *= num_list[i];
+        }
+        
+        sum = sum*sum;
+        
+        if(sum > mulSum){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
